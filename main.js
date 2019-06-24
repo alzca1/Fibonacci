8cff4b09a7cee6597c9180cb2fa1d91afdf0abb8
Alvaro Zajara <alzca1@gmail.com>
2019-06-24 17:33:49 +0200

    My first script

---
+++ main.js
@@ -0,0 +1,38 @@
+'use strict';
+
+
+var b = 1; 
+var a = 0; 
+var limit = 30; 
+
+// console.log(a);
+
+
+// if(limit > 0) {
+//     console.log(b);
+// }
+
+// while ( a + b <= limit) {
+
+//     var temp = b;
+//     b = a + b; 
+//     a = temp; 
+// ;
+
+// console.log(b);
+
+
+    
+// }
+
+
+
+
+for (var a = 0; a <= 0; a++){
+    console.log(a);
+    var temporary = a + b; 
+    a = b; 
+    b = temporary; 
+
+    
+}
